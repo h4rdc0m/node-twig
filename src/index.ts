7c3950286adf565fd3e336d9399d6a003c555995
Christiaan Benedictus <chris@combat.nl>
2022-12-07 11:58:41 +0100

    fixed imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { foo } from "./foo";
-import { bar } from "./bar";
+import {bar} from "./bar";
+import {foo} from "./foo";
 
 export function foobar(a: number, b: number) {
   return foo().repeat(a).length + bar().repeat(b).length;
 }
+

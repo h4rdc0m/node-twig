6b3389f1b26bc12ba28ef6eb3688a4236874c72a
Christiaan Benedictus <chris@combat.nl>
2022-12-07 12:16:03 +0100

    fix formatting
    Signed-off-by: Christiaan Benedictus <chris@combat.nl>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {bar} from "./bar";
-import {foo} from "./foo";
+import { bar } from "./bar";
+import { foo } from "./foo";
 
 export function foobar(a: number, b: number) {
   return foo().repeat(a).length + bar().repeat(b).length;

fbd477e99605cea06ad3a3405b1a879d06f0656a
Christiaan Benedictus <chris@combat.nl>
2022-12-07 11:56:23 +0100

    reformat

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,13 @@
-import { describe, expect, it} from "vitest";
+import { describe, expect, it } from "vitest";
 import { foobar } from "../index";
 
-describe('foobar', () => {
-  describe('given two positive integers', () => {
+describe("foobar", () => {
+  describe("given two positive integers", () => {
     const first = 1;
     const second = 2;
 
-    describe('when called', () => {
-      it('should return the sum of the two integers multiplied by 3', () => {
+    describe("when called", () => {
+      it("should return the sum of the two integers multiplied by 3", () => {
         expect(foobar(first, second)).toBe(9);
       });
     });
